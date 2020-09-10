d224709a06b3f493ab6e64131afd9cb5e2034495
danvarga1985 <danvarga1985@gmail.com>
2020-09-10 11:01:35 +0200

    Initial commit

---
+++ src/test/java/daniel/varga/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.varga.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
