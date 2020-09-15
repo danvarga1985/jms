dc29e0faca7194afb889463ef5fcb1dbd6837457
danvarga1985 <danvarga1985@gmail.com>
2020-09-15 16:31:15 +0200

    adding TaskConfig + serverconfig

---
+++ src/main/java/daniel/varga/jms/model/HelloWorldMessage.java
@@ -0,0 +1,17 @@
+package daniel.varga.jms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+    private UUID id;
+    private String message;
+}
